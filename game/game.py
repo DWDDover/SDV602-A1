021c4efaf08baf0710567d18fa6968c79c5b0b05
DWDDOVER <declan-dover@live.nmit.ac.nz>
2026-08-25 14:27:55 +1200

    changed the way locations are set up to have temporary strings as references

--- game/game.py
+++ game/game.py
@@ -8,4 +8,20 @@ class Game:
         self.player = Player()
         self.current_location = village
 
-village = Location("Village", "A small village consisting of some crude huts and farmland", {"north": forest, "east": lake, "south": cave_mouth}, "enemies" : [], "items" : [],)
+# locations created initially with string references
+village = Location("Village", "A small village consisting of some crude huts and farmland",
+                    {"north": "forest", "east": "lake", "south": "cave_mouth"}, [], [])
+
+forest = Location("Forest", "A dense dark forest",
+                   {"west": "castle", "east": "clearing", "south": "village"}, [], [])
+
+
+#location dictionary
+all_locations = {"village": village, "forest": forest}  # add the rest here
+
+#replace all the string values with the actual loaction objects
+for location in all_locations.values():
+    location.directions = {
+        direction: all_locations[name]
+        for direction, name in location.directions.items()
+    }

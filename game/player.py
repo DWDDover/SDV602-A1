a9c7c4f22290f83478569d7501759979ba315aeb
DWDDOVER <declan-dover@live.nmit.ac.nz>
2026-08-31 21:23:18 +1200

    chnages to game and gui

--- game/player.py
+++ game/player.py
@@ -1,9 +1,12 @@
+from inventory import Inventory
+
 class Player:
     def __init__(self):
         self.max_hp = 100
         self.current_hp = 100
         self.attack = 1
         self.defense = 1
+        self.inventory = Inventory()
         
     def __str__(self):
         return "HP: " + str(self.current_hp) + "/" + str(self.max_hp) + "\nAttack: " + str(self.attack) + "\nDefense: " + str(self.defense) + "\n"

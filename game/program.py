fc71c21c9ad2b711302e8e4ef296258cbcdc3304
DWDDover <Declan-Dover@live.nmit.ac.nz>
2026-09-02 12:44:19 +1200

    started writing the headless testing

--- game/program.py
+++ game/program.py
@@ -1,17 +1,30 @@
 from player import Player
 from game import Game
 from gui import GUI
+from command_parser import Command_parser
 
-#This fixes the display of the window on my PC for some reason
+#This fixes the display of the window on my home PC for some reason
 try:
     import ctypes
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
 except Exception:
     pass
 
-if __name__ == "__main__":
-    player = Player()
-    game = Game()
+HEADLESS = True
 
-    gui = GUI(game)
-    gui.run()
+if __name__ == "__main__":
+    game = Game()
+    
+    if not HEADLESS:
+        gui = GUI(game)
+        gui.run()
+        
+    else:
+        cp = Command_parser(game)
+        print(game.player)
+        print(game.current_location.description)
+        print(cp.parse("travel north"))
+        print(cp.parse("travel east"))
+        print(cp.parse("engage"))
+        print(game.player)
+        print(game.player.inventory.items[0].name)

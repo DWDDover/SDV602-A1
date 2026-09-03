3cc295a2f031cc6c4717344e5831dc311b8f8550
DWDDover <Declan-Dover@live.nmit.ac.nz>
2026-09-03 13:47:45 +1200

    removed excess reference task folder

--- reference_tasks/session_8/player.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    def __init__(self, health_max=100, health_current=100, damage=5, block=2):
-        self.health_max = health_max
-        self.health_current = health_current
-        self.damage = damage
-        self.block = block
